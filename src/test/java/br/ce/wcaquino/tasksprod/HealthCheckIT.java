375d8523dabe2b1c2509d6508ab7770cbec8c492
unknown <gabriel.avila@siriussoftware.com.br>
2022-02-25 16:59:40 -0300

    Health Check

---
+++ src/test/java/br/ce/wcaquino/tasksprod/HealthCheckIT.java
@@ -0,0 +1,32 @@
+package br.ce.wcaquino.tasksprod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class HealthCheckIT {
+
+	@Test
+	public void healtheCheck() throws MalformedURLException {
+
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+		try {
+			driver.navigate().to("http://172.28.112.1:9999/tasks");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			String version = driver.findElement(By.id("version")).getText();
+			Assert.assertTrue(version.startsWith("build"));
+		} finally {
+			driver.quit(); 
+		}
+
+	}
+
+}
